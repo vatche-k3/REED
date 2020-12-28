d96565726223b4a7fbe96b74fafa4af8ac044c1e
Vatche Kafafian <vatchek3@vatches-air.home>
2020-12-28 15:32:52 -0500

    Able to add nouns and verbs to a list

--- test/test.py
+++ test/test.py
@@ -3,9 +3,23 @@ nlp = spacy.load("en_core_web_sm")
 doc =  open('lorem.txt')
 
 data = doc.read()
-
 i = nlp(data)
 
+noun = []
+verb = []
+
 for token in i:
-    print(token.text, token.dep_, token.head.text, token.head.pos_,
-            [child for child in token.children])
+    pos = token.pos_
+    if(pos == "NOUN"):
+        noun.append(token.text)
+    elif(pos == "VERB"):
+        verb.append(token.text)
+
+    # print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+            # token.shape_, token.is_alpha, token.is_stop)
+
+for n in noun:
+    print(n)
+
+for v in verb:
+    print(v)
